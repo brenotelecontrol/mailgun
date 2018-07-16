fec2bc8074ab727125fa5fbb42ff159376935f3b
Breno Sabella <breno.sabella@telecontrol.com.br>
2018-07-16 17:11:48 -0300

    Logotipos no index

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <?php
     include 'menu.php';
+?><br><br><br><br><br><br>
+    <center>
+        <img src="https://telecontrol.com.br/images/logo.png" alt="Imagem Telecontrol" height="150" width="600" /><br><br><br>
+        <img src="https://kinsta.com/wp-content/uploads/2017/08/mailgun-wordpress.png" alt="Imagem Mailgun" height="150" width="600" /><br>
+    </center>
+<?php
     include 'rodape.php';
 ?>
